e02cd127cbf37c21b933b68d2aa04f40fd0e2faf
samandargofurov <samandargofurov236@gmail.com>
2024-02-01 21:58:18 +0500

    10 lesson

---
+++ index.js
@@ -0,0 +1,32 @@
+// for Each
+
+// let arr = [2, 4, 3, 5, 6, -31];
+
+// arr.forEach((v, i, arr) => {
+//     console.log(v);
+// })
+
+
+// 1. Berilgan sonlardan tashkil topgan massivdan 10 dan kattalaridan iborat yangi massiv hosil qiluvchi funksiya.
+
+let arr = [10, 213, 6, 7, 32, 12, 42, 756, 98, 2, 4, ];
+
+const arr1 = arr.map((value) => {
+    return value > 10;
+})
+
+console.log(arr1);
+
+// 2. Sonlardan tashkil topgan massiv elementlari kublaridan iborat yangi massiv hosil qiladigan funksiya yozing.
+
+
+
+// 3. Sonlardan tashkil topgan massiv elementlari orasidan 3 ga hamda 5 ga karrali son bor yoki yoqligini tekshiruvchi funksiya yozing.
+
+
+
+// 4. Sonlardan tashkil topgan massiv elementlari orasidan 3- eng kattasini topuvchi funksiya yozing.
+
+
+
+// 5. Sonlardan tashkil topgan massiv teskari tartibda yozib uni yozuv korinishiga o'tkazib beruvchi funksiya yozing.
