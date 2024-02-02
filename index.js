2731937fee732272decc8c19a359078c67b96731
samandargofurov <samandargofurov236@gmail.com>
2024-02-02 11:04:03 +0500

    10 lesson

--- index.js
+++ index.js
@@ -1,32 +1,50 @@
-// for Each
+// 1. Berilgan sonlardan tashkil topgan massivdan 10 dan kattalaridan iborat yangi massiv hosil qiluvchi funksiya. \/\/
 
-// let arr = [2, 4, 3, 5, 6, -31];
+// let arr = [10, 11, 21, 6, 7, 3, 1, 4, 76, 98, 2, 4, ];
 
-// arr.forEach((v, i, arr) => {
-//     console.log(v);
-// })
+// function ondanKattaSonlar(arg) {
+//     let newMassive = [];
+//     for (let i = 0; i < arg.length; i++) {
+//         if (arg[i] > 10) {
+//             newMassive.push(arg[i]);
+//         }
+//     }
+//     return newMassive
+// }
 
+// let res = ondanKattaSonlar(arr);
+// console.log(res);
 
-// 1. Berilgan sonlardan tashkil topgan massivdan 10 dan kattalaridan iborat yangi massiv hosil qiluvchi funksiya.
+// 2. Sonlardan tashkil topgan massiv elementlari kublaridan iborat yangi massiv hosil qiladigan funksiya yozing. \/\/
 
-let arr = [10, 213, 6, 7, 32, 12, 42, 756, 98, 2, 4, ];
+// let massive = [2, 3, 1, 7, 5, 6];
 
-const arr1 = arr.map((value) => {
-    return value > 10;
-})
+// let elmKubi = massive.map((value) => {
+//     return value ** 3
+// });
 
-console.log(arr1);
-
-// 2. Sonlardan tashkil topgan massiv elementlari kublaridan iborat yangi massiv hosil qiladigan funksiya yozing.
-
-
+// console.log(elmKubi);
 
 // 3. Sonlardan tashkil topgan massiv elementlari orasidan 3 ga hamda 5 ga karrali son bor yoki yoqligini tekshiruvchi funksiya yozing.
 
 
 
-// 4. Sonlardan tashkil topgan massiv elementlari orasidan 3- eng kattasini topuvchi funksiya yozing.
+// 4. Sonlardan tashkil topgan massiv elementlari orasidan 3-eng kattasini topuvchi funksiya yozing.
 
+// let arr = [2, 5, 4, 8, 7, 6, 3, 11, 10, 7, 13];
 
+// let arr1 = arr.sort((a, b) => {
+//     return b - a
+// })
+// console.log(arr1);
+// console.log(arr1[2]);
 
 // 5. Sonlardan tashkil topgan massiv teskari tartibda yozib uni yozuv korinishiga o'tkazib beruvchi funksiya yozing.
+
+let massive = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+let arr1 = massive.reverse().map((v) => {
+    return v.toString();
+})
+
+console.log(arr1);
